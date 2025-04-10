5a19d58bfc5b3ede30b90c172b1debcd2c2ca5df
jmnuf <jose.m.nufio.0@gmail.com>
2025-04-10 12:51:07 +0200

    [feat] Funcionalidad para actualizacion de libro hecha

--- actualizar-libro.php
+++ actualizar-libro.php
@@ -1,12 +1,16 @@
 <?php
+
+require_once(__DIR__ . '/config.php');
+
 $titulo = '';
 $autor = '';
 $disponible = '1';
 $id = '';
 
 $updated = false;
+$error = null;
 
-function extract($arr, $keys) {
+function extract_values($arr, $keys) {
   $data = [];
   foreach($keys as $k) {
     if (!isset($arr[$k])) return false;
@@ -15,15 +19,43 @@ function extract($arr, $keys) {
   return $data;
 }
 
-if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
   if (!isset($_GET['codigo'])) {
     header("Location: ./");
     die();
   }
+
+  $codigo = $_GET['codigo'];
+  $stmt = mysqli_prepare($conexion, 'SELECT * FROM libros WHERE codigo=?');
+  mysqli_stmt_bind_param($stmt, 'i', $codigo);
+  if (!mysqli_stmt_execute($stmt)) {
+    $error = mysqli_stmt_error($stmt);
+  } else {
+    $id = $codigo;
+    $q = mysqli_stmt_get_result($stmt);
+    while ($libro = mysqli_fetch_array($q)) {
+      $id = $libro['codigo'];
+      $titulo = $libro['titulo'];
+      $autor = $libro['autor'];
+      $disponible = $libro['disponible'];
+    }
+  }
+  mysqli_stmt_close($stmt);
+  
 } else {
-  $data = extract($_POST, ['id', 'titulo', 'autor', 'disponible']);
+  $data = extract_values($_POST, ['id', 'titulo', 'autor', 'disponible']);
   if ($data !== false) {
-    $updated = true;
+    $id = $data['id'];
+    $titulo = $data['titulo'];
+    $autor = $data['autor'];
+    $disponible = $data['disponible'];
+
+    $stmt = mysqli_prepare($conexion, 'UPDATE libros SET titulo=?, autor=?, disponible=? WHERE codigo=?');
+    mysqli_stmt_bind_param($stmt, 'ssii', $titulo, $autor, $disponible, $codigo);
+    $updated = mysqli_stmt_execute($stmt);
+    if (!$updated) {
+      $error = mysqli_stmt_error($stmt);
+    }
   }
 }
 
@@ -37,28 +69,49 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     <link rel="stylesheet" href="./css/formularios.css" />
   </head>
   <body>
-    <form class="actualizar-form">
-      <input type="hidden" name="id" value="<?= $id ?>" />
-      <label>
-        Nuevo Titulo de Libro:
-      </label>
-      <input type="text" name="titulo" value="<?= $titulo ?>" />
+    <?php
+    if ($error != null) {
+      echo '<p class="sql-error">', htmlespecialchars($error), '</p>';
+    }
+    ?>
+    <?php
+    if ($updated) {
+      ?>
 
-      <label>
-        Nuevo Autor de Libro:
-      </label>
-      <input type="text" name="autor" value="<?= $autor ?>" />
+      <div class="actualizado">
+        <p>Libro actualizado</p>
+        <a href="./">Regresar a inicio</a>
+      </div>
+      
+      <?php
+    } else {
+    ?>
+      <form method="POST" class="actualizar-form">
+        <input type="hidden" name="id" value="<?= $id ?>" />
 
-      <label>
-        Disponible:
-      </label>
-      <select>
-        <option value="0" <?= $disponible == false ? 'selected' : '' ?>>No</option>
-        <option value="1" <?= $disponible == true ? 'selected' : '' ?>>Sí</option>
-      </select>
+        <label>
+          Nuevo Titulo de Libro:
+        </label>
+        <input type="text" name="titulo" value="<?= $titulo ?>" />
 
-      <button>Actualizar</button>
-    </form>
+        <label>
+          Nuevo Autor de Libro:
+        </label>
+        <input type="text" name="autor" value="<?= $autor ?>" />
+
+        <label>
+          Disponible:
+        </label>
+        <select name="disponible">
+          <option value="0" <?= $disponible == false ? 'selected' : '' ?>>No</option>
+          <option value="1" <?= $disponible == true ? 'selected' : '' ?>>Sí</option>
+        </select>
+
+        <button type="submit">Actualizar</button>
+      </form>
+    <?php
+    }
+    ?>
   </body>
 </html>
 
